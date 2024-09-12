b754d03a9c976343acddbd056c9c08057081519c
aman-srivastav24 <shinchanmusic24@gmail.com>
2024-09-13 00:22:40 +0530

    First PHP Program

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP First Program</title>
+</head>
+<body>
+    <?php 
+    echo "Welcome PHP";
+    echo "<br> So Let's Do this";
+    $a=24;$b="Aman";
+    echo $a;
+    print($b)
+    ?>
+</body>
+</html>
